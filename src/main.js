edc6829fea2a6fda8a0f611c0b12e30f35bc747d
goregora <104126170+goregora@users.noreply.github.com>
2023-02-05 21:44:33 +0300

    Получение данных от сервера ч.1

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import TravelRoutePresenter from './presenter/travel-route-presenter.js';
 import PointModel from './model/travel-route-model.js';
-import { getPoints, getDestinations, getOffers, getOffersByType } from './mock/travel-route-mock.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import FilterModel from './model/filter-model.js';
+import PointApiService from './network.js';
 
 const filtersPosition = document.querySelector('.trip-controls__filters');
 const contentContainer = document.querySelector('.trip-events');
 
-const points = getPoints();
-const destinations = getDestinations();
-const offers = getOffers();
-const offersByType = getOffersByType();
+const URL = 'https://19.ecmascript.pages.academy/big-trip-simple';
+const TOKEN = 'Basic u834u3r32j';
 
-const pointModel = new PointModel({points, destinations, offers, offersByType});
-const filterModel = new FilterModel();
-const travelRoutePresenter = new TravelRoutePresenter(contentContainer, pointModel, filterModel);
-const filterPresenter = new FilterPresenter(filtersPosition, filterModel, travelRoutePresenter);
-//debugger;
-travelRoutePresenter.init();
-filterPresenter.init();
+const pointsApiService = new PointApiService(URL, TOKEN);
+const pointModel = new PointModel(pointsApiService);
+// const filterModel = new FilterModel();
+// const travelRoutePresenter = new TravelRoutePresenter(contentContainer, pointModel, filterModel);
+// const filterPresenter = new FilterPresenter(filtersPosition, filterModel, travelRoutePresenter);
+
+// travelRoutePresenter.init();
+// filterPresenter.init();
+pointModel.points.then((data) => console.log(data));
--- src/model/travel-route-model.js
+++ src/model/travel-route-model.js
@@ -2,80 +2,71 @@ import { FilterValues } from '../const';
 import dayjs from 'dayjs';
 
 export default class PointModel {
-  #points = null;
-  #destinations = null;
-  #offers = null;
-  #offersByType = null;
+  #pointsApiService = null;
 
-  constructor({points, destinations, offers, offersByType}) {
-    this.#points = points;
-    this.#destinations = destinations;
-    this.#offers = offers;
-    this.#offersByType = offersByType;
-    this.#compareOffersByType();
-    this.#comparePoints();
+  constructor(pointsApiService) {
+    this.#pointsApiService = pointsApiService;
   }
 
-  #comparePoints() {
-    for (let i = 0; i < this.#points.length; i++) {
-      this.#points[i].destination = this.#destinations.find((destination) => this.#points[i].destination === destination.id);
-      for (let j = 0; j < this.#points[i].offers.length; j++) {
-        this.#points[i].offers[j] = this.#offers.find((offer) => this.#points[i].offers[j] === offer.id);
-      }
-    }
-  }
+  // #comparePoints() {
+  //   for (let i = 0; i < this.#points.length; i++) {
+  //     this.#points[i].destination = this.#destinations.find((destination) => this.#points[i].destination === destination.id);
+  //     for (let j = 0; j < this.#points[i].offers.length; j++) {
+  //       this.#points[i].offers[j] = this.#offers.find((offer) => this.#points[i].offers[j] === offer.id);
+  //     }
+  //   }
+  // }
 
-  #compareOffersByType() {
-    for (let i = 0; i < this.#offersByType.length; i++) {
-      for (let j = 0; j < this.#offersByType[i].offers.length; j++) {
-        this.#offersByType[i].offers[j] = this.#offers.find((offer) => this.#offersByType[i].offers[j] === offer.id);
-      }
-    }
-  }
-
-  get destinations() {
-    return this.#destinations;
-  }
+  // #compareOffersByType() {
+  //   for (let i = 0; i < this.#offersByType.length; i++) {
+  //     for (let j = 0; j < this.#offersByType[i].offers.length; j++) {
+  //       this.#offersByType[i].offers[j] = this.#offers.find((offer) => this.#offersByType[i].offers[j] === offer.id);
+  //     }
+  //   }
+  // }
 
   get points() {
-    return this.#points;
+    return this.#pointsApiService.points;
   }
-
-  get offersByTypes() {
-    return this.#offersByType;
-  }
-
-  set points (points) {
-    this.#points = points;
-  }
-
-  filterPoints = (filterValue) => {
-    const now = dayjs();
-
-    if (filterValue === FilterValues.PAST) {
-      return this.#points.filter((pointData) => dayjs(pointData.dateFrom) < now);
-    }
-
-    if (filterValue === FilterValues.FUTURE) {
-      return this.#points.filter((pointData) => dayjs(pointData.dateFrom) > now);
-    }
-
-    return this.#points;
-  };
-
-  updatePoint = (pointData) => {
-    this.#points.find((point, index) => {
-      if (point.id === pointData.id) {
-        this.#points[index] = pointData;
-      }
-    });
-  };
-
-  deletePoint = (pointdata) => {
-    this.#points.splice((this.#points.indexOf(pointdata)), 1);
-  };
-
-  addPoint = (pointData) => {
-    this.#points.push(pointData);
-  };
 }
+//  get destinations() {
+//     return this.#destinations;
+//   }
+//   get offersByTypes() {
+//     return this.#offersByType;
+//   }
+
+//   set points (points) {
+//     this.#points = points;
+//   }
+
+//   filterPoints = (filterValue) => {
+//     const now = dayjs();
+
+//     if (filterValue === FilterValues.PAST) {
+//       return this.#points.filter((pointData) => dayjs(pointData.dateFrom) < now);
+//     }
+
+//     if (filterValue === FilterValues.FUTURE) {
+//       return this.#points.filter((pointData) => dayjs(pointData.dateFrom) > now);
+//     }
+
+//     return this.#points;
+//   };
+
+//   updatePoint = (pointData) => {
+//     this.#points.find((point, index) => {
+//       if (point.id === pointData.id) {
+//         this.#points[index] = pointData;
+//       }
+//     });
+//   };
+
+//   deletePoint = (pointdata) => {
+//     this.#points.splice((this.#points.indexOf(pointdata)), 1);
+//   };
+
+//   addPoint = (pointData) => {
+//     this.#points.push(pointData);
+//   };
+// }
