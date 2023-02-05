34cb858e4d1bd1ef6c2845bc77e9b6de4d772001
goregora <104126170+goregora@users.noreply.github.com>
2023-02-05 11:30:21 +0300

    добавление новой точки ч.3

--- src/view/add-point.js
+++ src/view/add-point.js
@@ -11,6 +11,20 @@ function createEventTypeTemplate(pointType) {
     </div>`).join('');
 }
 
+function createOffersListTemplate(point, offersByType) {
+  const offerType = offersByType.find((offer) => offer.type === point.type);
+  return offerType.offers.map((offer, index) => `
+        <div class="event__offer-selector">
+          <input class="event__offer-checkbox  visually-hidden" id="event-offer-${point.id}-${offer.id}-${index}" type="checkbox" name="event-offer-${point.id}-${offer.id}"${isIncludes(point.offers, offer.id) ? 'checked' : ''}>
+          <label class="event__offer-label" for="event-offer-${point.id}-${offer.id}-${index}">
+            <span class="event__offer-title">${offer.title}</span>
+            &plus;&euro;&nbsp;
+            <span class="event__offer-price">${offer.price}</span>
+          </label>
+        </div>`)
+    .join('');
+}
+
 function createDestinationOptions(destinations) {
   return destinations.map((destination) => `<option value="${destination.name}"></option>`).join('');
 }
@@ -47,10 +61,10 @@ function createAddPointTemplate(destinations, point) {
 
               <div class="event__field-group  event__field-group--time">
                 <label class="visually-hidden" for="event-start-time">From</label>
-                <input class="event__input  event__input--time" id="event-start-time" type="text" name="event-start-time" value="--/--/-- --:--">
+                <input class="event__input  event__input--time" id="event-start-time" type="text" name="event-start-time" value="${point.dateFrom}">
                 &mdash;
                 <label class="visually-hidden" for="event-end-time">To</label>
-                <input class="event__input  event__input--time" id="event-end-time" type="text" name="event-end-time" value="05/02/23 12:00">
+                <input class="event__input  event__input--time" id="event-end-time" type="text" name="event-end-time" value="${point.dateTo}">
               </div>
 
               <div class="event__field-group  event__field-group--price">
@@ -58,7 +72,7 @@ function createAddPointTemplate(destinations, point) {
                   <span class="visually-hidden">Price</span>
                   &euro;
                 </label>
-                <input class="event__input  event__input--price" id="event-price" type="text" name="event-price" value="">
+                <input class="event__input  event__input--price" id="event-price" type="text" name="event-price" value="${point.basePrice}">
               </div>
 
               <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -77,52 +91,16 @@ function createAddPointTemplate(destinations, point) {
                       <span class="event__offer-price">30</span>
                     </label>
                   </div>
-
-                  <div class="event__offer-selector">
-                    <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-                    <label class="event__offer-label" for="event-offer-comfort-1">
-                      <span class="event__offer-title">Switch to comfort class</span>
-                      &plus;&euro;&nbsp;
-                      <span class="event__offer-price">100</span>
-                    </label>
-                  </div>
-
-                  <div class="event__offer-selector">
-                    <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-                    <label class="event__offer-label" for="event-offer-meal-1">
-                      <span class="event__offer-title">Add meal</span>
-                      &plus;&euro;&nbsp;
-                      <span class="event__offer-price">15</span>
-                    </label>
-                  </div>
-
-                  <div class="event__offer-selector">
-                    <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-                    <label class="event__offer-label" for="event-offer-seats-1">
-                      <span class="event__offer-title">Choose seats</span>
-                      &plus;&euro;&nbsp;
-                      <span class="event__offer-price">5</span>
-                    </label>
-                  </div>
-
-                  <div class="event__offer-selector">
-                    <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-                    <label class="event__offer-label" for="event-offer-train-1">
-                      <span class="event__offer-title">Travel by train</span>
-                      &plus;&euro;&nbsp;
-                      <span class="event__offer-price">40</span>
-                    </label>
-                  </div>
                 </div>
               </section>
 
               <section class="event__section  event__section--destination">
                 <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-                <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+                <p class="event__destination-description">${description}</p>
 
                 <div class="event__photos-container">
                   <div class="event__photos-tape">
-                    <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
+                    <img class="event__photo" src="${srcphoto}" alt="Event photo">
                     <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
                     <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
                     <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
@@ -138,7 +116,10 @@ export default class AddPointView extends AbstractStatefulView {
   #destinations = null;
   #pointData = {
     destination: '',
-    type: ''
+    type: 'taxi',
+    dateFrom: '',
+    dateTo: '',
+    basePrice: ''
   };
 
   #dateFromPicker = null;
@@ -157,13 +138,11 @@ export default class AddPointView extends AbstractStatefulView {
   #setDestinationHandler = (evt) => {
     evt.preventDefault();
     this.updateElement({destination: evt.target.value});
-    console.log(this._state);
   };
 
   #setBasePriceHandler = (evt) => {
     evt.preventDefault();
-    this._state.basePrice = evt.target.value;
-    this.updateElement(this._state);
+    this.updateElement({basePrice: evt.target.value});
   };
 
   #eventTypeHandler = (evt) => {
